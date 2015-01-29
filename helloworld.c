27ec294ee806cbb869e7287ba5ceb6cbac1c5873
Jeremie <13520277@gm.uit.edu.vn>
2015-01-30 00:50:46 +0700

    RESET no-arg jump back 2 step

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 
 int main(){
-	printf("Welcome to Linux Training Campaign!!! ^^\n");
+<<<<<<< HEAD
+	printf("Welcome to Linux Training Campaign!!! ^^\n"); // nen sua lại cai nay
     printf("Say Hi!");
+    // thieu return
+=======
+	printf("Welcome to Linux Training Campaign!!! ^^\n"); // say hello
+    printf("Say Hi!");
+    // this is soething should be confilct!!
+    //yea!
+>>>>>>> fix-1
 }
