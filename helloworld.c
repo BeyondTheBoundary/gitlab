b1800af0efc3e35dfc0ae49e049949fac180422c
Jeremie <13520277@gm.uit.edu.vn>
2015-01-29 22:42:14 +0700

    Make a Bug

--- helloworld.c
+++ helloworld.c
@@ -2,4 +2,8 @@
 
 int main(){
 	printf("Welcome to Linux Training Campaign!!! ^^\n");
+    printf("Trolling code");l;asdasdjasl;jdlkjal;sd;lk;lkjkopscfd
+    dsakf;óa;fjo';ôạop'okj;dsfa;k'fsdjpo'fasd'djp'pjfsdaj'ojfpodsdspadspojfDSdpjofsdojfsd;odsfjopfDSoj
 }
+// Spataaaaaaaaaaaa!!!@!!!!
+// Spataaaaaaaaaaaa!!!@!!!!
